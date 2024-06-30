46b671983cdad12b5e9b935aff7111cc70836310
Dan Dyar <dan@parameterz.com>
2024-06-30 16:44:46 -0700

    added media files

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Next.js App</title>
+        <title>Visual Protocol App (Next.js)</title>
       </head>
       <body>
         <header>
