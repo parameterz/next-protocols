61b06f5e8fdb1e6a46395f31c3419e62b7fce1b1
Dan Dyar <dan@parameterz.com>
2024-06-30 16:51:07 -0700

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -4,6 +4,11 @@ const configInfo = {
     author: 'Dan Dyar, MA, RDCS, FASE',
     versions: [
       {
+        date: "6/30/24; 4:50PM",
+        version: "0.1.3",
+        notes: "added the remainder of the media files"
+      },
+      {
         date: "6/30/24; 3:12 PM",
         version: "0.1.2",
         notes: "refactored version, LAST_UPDATED"
