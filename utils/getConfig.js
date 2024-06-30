2c0fd171e755e7d8a66991d26f8ca76868484607
Dan Dyar <dan@parameterz.com>
2024-06-30 15:16:35 -0700

    refactored build/versioning logs

--- utils/getConfig.js
+++
@@ -1,9 +0,0 @@
-// utils/getConfig.js
-export const getConfig = () => {
-    return {
-      buildVersion: '0.1.0',
-      buildDate: '2024-06-29 23:00',
-      author: 'Dan Dyar, MA, RDCS, FASE'
-    };
-  };
-  
