a730192b6e2a213e962aeae32cbf7c502a84deac
Dan Dyar <dan@parameterz.com>
2024-06-30 12:36:35 -0700

    Update next.config.mjs
    advice from bro: output to .next

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    //output: 'export',  
     trailingSlash: true,
     images: {
       unoptimized: true,
