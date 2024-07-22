90aeab0b4976a42da8017e7cef6d42f83da90295
Dan Dyar <dan@parameterz.com>
2024-07-21 23:23:34 -0700

    more 115 protocols

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>Visual Protocol App (Next.js)</title>
+        <title>GC Visual Protocol App</title>
 
       </head>
       <body>
--- protocols/protocols.js
+++ protocols/protocols.js
@@ -2,13 +2,17 @@
 
 import canonicalProtocol from './canonicalProtocol';
 import cvt_115_exam1 from './cvt_115_exam1';
+import cvt_115_exam2 from './cvt_115_exam2';
+import cvt_115_exam3 from './cvt_115_exam3';
 import cvt_221_parasternals from './cvt_221_parasternals';
 import cvt_221_parast_apicals from './cvt_221_parast_apicals';
 import cvt_221_subcostals from './cvt_221_subcostals';
 
 const protocolsConfig = [
   { name: 'GC Full Protocol', file: 'canonicalProtocol', data: canonicalProtocol },
-  { name: '115 Parasternal ONLY', file: 'cvt_115_exam1', data: cvt_115_exam1 },
+  { name: '115 Exam 1 (Parasternals,2D only)', file: 'cvt_115_exam1', data: cvt_115_exam1, note: 'based on spring 2024; created summer 2024' },
+  { name: '115 Exam 2 (PLAX & PSAX)', file: 'cvt_115_exam2', data: cvt_115_exam2, note: 'based on spring 2024; created summer 2024' },
+  { name: '115 Exam 3 (Final; Parasternals & A4C series)', file: 'cvt_115_exam3', data: cvt_115_exam3, note: 'based on spring 2024; created summer 2024' },
   { name: '221 Parasternals (PLAX & PSAX)', file: 'cvt_221_parasternals', data: cvt_221_parasternals, note: 'based on fall 2023; created summer 2024' },
   { name: '221 Parasternal & Apicals', file: 'cvt_221_parast_apicals', data: cvt_221_parast_apicals, note: 'based on fall 2023; created summer 2024'},
   { name: '221 Subcostals Only', file: 'cvt_221_subcostals', data: cvt_221_subcostals, note: 'based on fall 2023; created summer 2024' }
