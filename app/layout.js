975cbb5398d46a99fc1a2328895c848dc3492999
Dan Dyar <dan@parameterz.com>
2025-01-27 07:13:21 -0800

    updated title

--- app/layout.js
+++ app/layout.js
@@ -10,12 +10,12 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>GC Visual Protocol App</title>
+        <title>Visual Protocol App</title>
 
       </head>
       <body>
         <header>
-        <h1>GC Visual Protocol</h1>
+        <h1>Visual Protocol</h1>
         </header>
         <main>{children}</main>
       </body>
