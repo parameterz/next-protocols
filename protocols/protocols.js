0190f01c06adc48a84e37859a95380f8fbbc8836
Dan Dyar <dan@parameterz.com>
2025-01-27 11:51:57 -0800

    Update protocols.js

--- protocols/protocols.js
+++ protocols/protocols.js
@@ -9,8 +9,8 @@ import cvt_221_parast_apicals from './cvt_221_parast_apicals';
 import cvt_221_subcostals from './cvt_221_subcostals';
 
 const protocolsConfig = [
-  { name: 'GC Full Protocol', file: 'canonicalProtocol', data: canonicalProtocol },
-  { name: '115 Exam 1 (Parasternals,2D only)', file: 'cvt_115_exam1', data: cvt_115_exam1, note: 'based on spring 2024; created summer 2024' },
+  { name: 'Full Adult Protocol', file: 'canonicalProtocol', data: canonicalProtocol },
+  { name: 'Parasternals,2D only', file: 'cvt_115_exam1', data: cvt_115_exam1, note: 'based on spring 2024; created summer 2024' },
   { name: '115 Exam 2 (PLAX & PSAX)', file: 'cvt_115_exam2', data: cvt_115_exam2, note: 'based on spring 2024; created summer 2024' },
   { name: '115 Exam 3 (Final; Parasternals & A4C series)', file: 'cvt_115_exam3', data: cvt_115_exam3, note: 'based on spring 2024; created summer 2024' },
   { name: '221 Parasternals (PLAX & PSAX)', file: 'cvt_221_parasternals', data: cvt_221_parasternals, note: 'based on fall 2023; created summer 2024' },
